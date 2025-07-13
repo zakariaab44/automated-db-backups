36b4a8debfb91d21cf81751d8ebd94eb5f612d8f
zakariaab44 <zab03304@gmail.com>
2025-07-13 23:51:04 +0100

    everything done

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -1,17 +1,12 @@
 #! /bin/bash
+source .env
 
-echo "[mongodb-org-8.0]
-name=MongoDB Repository
-baseurl=https://repo.mongodb.org/yum/amazon/2023/mongodb-org/8.0/x86_64/
-gpgcheck=1
-enabled=1
-gpgkey=https://pgp.mongodb.com/server-8.0.asc" > /etc/yum.repos.d/mongodb-org-8.0.repo
+sudo yum install -y docker
+sudo systemctl start docker
+sudo systemctl enable docker
 
-sudo yum install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
+sudo docker run -d --name mongodb  -p 27017:27017 -v /etc/mongo  -e MONGO_INITDB_ROOT_USERNAME="$MONGO_USER" -e MONGO_INITDB_ROOT_PASSWORD="$MONGO_SECRET"  mongo:8.0 --bind_ip_all
 
 curl -O https://fastdl.mongodb.org/tools/db/mongodb-database-tools-amazon2-x86_64-100.9.4.tgz
 tar -zxvf mongodb-database-tools-amazon2-x86_64-100.9.4.tgz
 sudo cp mongodb-database-tools-*/bin/* /usr/local/bin/
-
